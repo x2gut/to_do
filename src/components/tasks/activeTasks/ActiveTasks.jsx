6f500102665725ca055372376208530bc668220e
x2gut <zorganhux@gmail.com>
2024-09-02 19:12:43 +0200

    simplify logic

--- src/components/tasks/activeTasks/ActiveTasks.jsx
+++ src/components/tasks/activeTasks/ActiveTasks.jsx
@@ -1,116 +1,141 @@
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { AiFillEdit, AiOutlineDelete } from "react-icons/ai";
+import ConfettiExplosion from "react-confetti-explosion";
 
-import divideArrayIntoChunks from "../../../utils/divideArray";
 import "./activeTasks.css";
-import LoadMoreBtn from "../../loadMoreBtn/LoadMoreBtn";
-import Modal from "../../modal/Modal";
-import { deleteActiveTaskRequest } from "../../../utils/requests/tasks";
-import { validateToken } from "../../../utils/requests/validateToken";
-import { getToken } from "../../../utils/jwt/tokenManipulation";
-import { updateTaskRequest } from "../../../utils/requests/tasks";
+import {
+  completeTaskRequest,
+  deleteActiveTaskRequest,
+  updateTaskRequest,
+} from "../../../utils/requests/tasks";
+import { validateToken } from "../../../utils/requests/validateToken.js";
+import { getToken } from "../../../utils/jwt/tokenManipulation.js";
+import Modal from "../../modal/Modal.jsx";
+import divideArrayIntoChunks from "../../../utils/divideArray.js";
+import LoadMoreBtn from "../../loadMoreBtn/LoadMoreBtn.jsx"
 
-function ActiveTasks({
-  tasksAmount,
-  onComplete,
-  tasks,
-  activeButton,
-  onDelete,
-}) {
-  const dividedTasks = divideArrayIntoChunks(tasks, 10);
-  const [chunksToDisplay, setChunkstoDisplay] = useState(1);
-  const [elementsToDisplay, setElementsToDisplay] = useState([]);
+function ActiveTasks({ tasksData, setTasksData, activeButton }) {
   const [showModal, setShowModal] = useState(false);
-  const [indexToUpdate, setIndexToUpdate] = useState(null);
   const [inputData, setInputData] = useState("");
+  const [idToUpdate, setIdToUpdate] = useState(null);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [currentLevel, setCurrentLevel] = useState(1);
 
-  useEffect(() => {
-    const getFlattenedChunks = (chunksArray, chunksToDisplay) => {
-      return chunksArray.slice(0, chunksToDisplay).flat();
-    };
+  const dividedArray = divideArrayIntoChunks(tasksData.active, 10);
 
-    const updatedElements = getFlattenedChunks(dividedTasks, chunksToDisplay);
-
-    setElementsToDisplay(updatedElements);
-  }, [tasks, chunksToDisplay]);
-
-  const onSumbit = async () => {
+  const handleTaskDelete = async (id) => {
     const token = getToken();
-    const isTokenValid = await validateToken(token);
+    const isTokenValid = validateToken(token);
     if (isTokenValid) {
-      updateTaskRequest(tasks[indexToUpdate], inputData);
-      const updatedTasks = [...elementsToDisplay];
-      updatedTasks[indexToUpdate] = inputData;
-      setElementsToDisplay(updatedTasks);
-      setShowModal(false);
+      await deleteActiveTaskRequest(id);
+
+      setTasksData((prevData) => ({
+        ...prevData,
+        active: prevData.active.filter((task) => task.id !== id),
+      }));
     } else {
-      console.error("Error to update task");
+      console.error("Can't validate token");
     }
   };
 
-  showModal
-    ? (document.body.style.overflow = "hidden")
-    : (document.body.style.overflow = "");
+  const handleTaskComplete = async (id) => {
+    const token = getToken();
+    const isTokenValid = await validateToken(token);
+    if (isTokenValid) {
+      await completeTaskRequest(id);
+      const completedTask = tasksData.active.find((task) => task.id === id);
+
+      setTasksData((prevData) => ({
+        ...prevData,
+        active: prevData.active.filter((task) => task.id !== id),
+        completed: [
+          ...prevData.completed,
+          { ...completedTask, is_completed: true },
+        ],
+      }));
+      setShowConfetti(true);
+
+      setTimeout(() => {
+        setShowConfetti(false);
+      }, 2200);
+    }
+  };
+
+  const handleModalSubmit = async (id, newTask) => {
+    const token = getToken();
+    const isTokenValid = validateToken(token);
+    if (isTokenValid) {
+      await updateTaskRequest(id, newTask);
+
+      setTasksData((prevData) => ({
+        ...prevData,
+        active: prevData.active.map((task) =>
+          task.id === id ? { ...task, description: newTask } : task
+        ),
+      }));
+
+      setShowModal(false);
+    } else {
+      console.error("Can't validate token");
+    }
+  };
+
+  const handleShowMoreClick = () => {
+    setCurrentLevel((prevLevel) => Math.min(prevLevel + 1, dividedArray.length));
+  };
 
   return (
-    <div
-      className={`active-tasks ${activeButton === "active" ? "active" : ""}`}
-    >
-      <p className="total-tasks">Tasks to do: {tasksAmount}</p>
+    <div className={`active-tasks ${activeButton === "active" ? "active" : ""}`}>
+      <p className="total-tasks">Tasks to do: {tasksData.active.length}</p>
       <ul className="tasks-list">
-        {showModal ? (
-          <Modal
-            showModal={showModal}
-            setShowModal={setShowModal}
-            onSubmit={onSumbit}
-            setInputData={setInputData}
-          />
-        ) : null}
-        {elementsToDisplay.map((item, index) => (
-          <li key={index}>
-            {item}
-            <div className="manipulate-btn">
-              <button className="edit-task">
-                <AiFillEdit
-                  onClick={() => {
+        {dividedArray.slice(0, currentLevel).map((chunk, index) => (
+          <React.Fragment key={index}>
+            {chunk.map((task) => (
+              <li key={task.id}>
+                {task.description}
+                <div className="manipulate-btn">
+                  <button className="edit-task" onClick={() => {
                     setShowModal(!showModal);
-                    setIndexToUpdate(index);
-                  }}
-                  size={21}
-                />
-              </button>
-              <button className="delete-task">
-                <AiOutlineDelete
-                  onClick={() => {
-                    onDelete(index);
-                    deleteActiveTaskRequest(tasks[index]);
-                  }}
-                  size={21}
-                />
-              </button>
-              <button className="complete-task">
-                <svg
-                  onClick={() => onComplete(index)}
-                  width="18"
-                  height="13"
-                  viewBox="0 0 18 13"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M17.7851 1.67391L6.7851 12.6739C6.72125 12.7378 6.64542 12.7885 6.56196 12.8231C6.4785 12.8577 6.38904 12.8755 6.29869 12.8755C6.20834 12.8755 6.11888 12.8577 6.03542 12.8231C5.95196 12.7885 5.87614 12.7378 5.81229 12.6739L0.999785 7.86141C0.870782 7.7324 0.798309 7.55744 0.798309 7.375C0.798309 7.19256 0.870782 7.0176 0.999785 6.88859C1.12879 6.75959 1.30375 6.68712 1.48619 6.68712C1.66863 6.68712 1.84359 6.75959 1.9726 6.88859L6.29869 11.2155L16.8123 0.701094C16.9413 0.572091 17.1163 0.499619 17.2987 0.499619C17.4811 0.499619 17.6561 0.572091 17.7851 0.701094C17.9141 0.830097 17.9866 1.00506 17.9866 1.1875C17.9866 1.36994 17.9141 1.5449 17.7851 1.67391Z"
-                    fill="#9E78CF"
+                    setIdToUpdate(task.id);
+                  }}>
+                    <AiFillEdit size={21} />
+                  </button>
+                  <button className="delete-task" onClick={() => handleTaskDelete(task.id)}>
+                    <AiOutlineDelete size={21} />
+                  </button>
+                  <button className="complete-task" onClick={() => handleTaskComplete(task.id)}>
+                    <svg
+                      width="18"
+                      height="13"
+                      viewBox="0 0 18 13"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M17.7851 1.67391L6.7851 12.6739C6.72125 12.7378 6.64542 12.7885 6.56196 12.8231C6.4785 12.8577 6.38904 12.8755 6.29869 12.8755C6.20834 12.8755 6.11888 12.8577 6.03542 12.8231C5.95196 12.7885 5.87614 12.7378 5.81229 12.6739L0.999785 7.86141C0.870782 7.7324 0.798309 7.55744 0.798309 7.375C0.798309 7.19256 0.870782 7.0176 0.999785 6.88859C1.12879 6.75959 1.30375 6.68712 1.48619 6.68712C1.66863 6.68712 1.84359 6.75959 1.9726 6.88859L6.29869 11.2155L16.8123 0.701094C16.9413 0.572091 17.1163 0.499619 17.2987 0.499619C17.4811 0.499619 17.6561 0.572091 17.7851 0.701094C17.9141 0.830097 17.9866 1.00506 17.9866 1.1875C17.9866 1.36994 17.9141 1.5449 17.7851 1.67391Z"
+                        fill="#9E78CF"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                {showModal && (
+                  <Modal
+                    showModal={showModal}
+                    setShowModal={setShowModal}
+                    setInputData={setInputData}
+                    onSubmit={() => handleModalSubmit(idToUpdate, inputData)}
                   />
-                </svg>
-              </button>
-            </div>
-          </li>
+                )}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
-      {dividedTasks.length > chunksToDisplay && (
-        <LoadMoreBtn
-          onClick={() => setChunkstoDisplay((prevChunk) => prevChunk + 1)}
-        />
+      {currentLevel < dividedArray.length && (
+        <LoadMoreBtn onClick={handleShowMoreClick} />
+      )}
+      {showConfetti && (
+        <ConfettiExplosion force={0.4} particleCount={30} width={400} />
       )}
     </div>
   );
