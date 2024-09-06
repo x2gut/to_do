687ac973a1d7e442231709385b9a79d95b65748f
x2gut <zorganhux@gmail.com>
2024-09-06 03:17:24 +0200

    import changes

--- src/utils/routes/publicRoute/publicRoute.jsx
+++ src/utils/routes/publicRoute/publicRoute.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
-import { validateToken } from "../../requests/validateToken";
+import { validateToken } from "../../api/validateToken";
 
 function PublicRoute() {
   const [isAuth, setIsAuth] = useState(null);
