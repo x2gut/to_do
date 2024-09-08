65e5729958d19b2370f52b6534cb98be5ac2d3e3
x2gut <zorganhux@gmail.com>
2024-09-08 23:13:24 +0200

    strict mode removed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "react-day-picker/style.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
